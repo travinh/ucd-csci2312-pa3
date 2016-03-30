4a67eeaaeebfe792210b08e176896f0cf149556d
Tran <vinh.v.tran@ucdenver.edu>
2016-03-30 16:06:34 -0600

    Trouble with the  initial centroid

--- main.cpp
+++ main.cpp
@@ -58,8 +58,8 @@ int main() {
     test_cluster_SAO(ec, NumIters);
     test_cluster_centroid(ec, NumIters);
     test_cluster_id(ec, NumIters);
-    test_cluster_initselection(ec, NumIters);
-    test_cluster_IO(ec, NumIters);
+    //test_cluster_initselection(ec, NumIters);
+    //test_cluster_IO(ec, NumIters);
 
     // kmeans tests
 //    test_kmeans_smoketest(ec);
